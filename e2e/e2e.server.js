29edb00a93e2d6b09c99aea53dfe62caa21638ba
Anna Popova <panasenko-anna.8@mail.ru>
2023-01-05 11:39:15 +0300

    fix e2e server

--- e2e/e2e.server.js
+++ e2e/e2e.server.js
@@ -2,7 +2,8 @@ const webpack = require("webpack");
 const WebpackDevServer = require("webpack-dev-server");
 const config = require("../webpack.dev");
 
-const server = new WebpackDevServer(webpack(config), {});
+const server = new WebpackDevServer({ ...config.devServer }, webpack(config));
+
 server.listen(9000, "localhost", (err) => {
   if (err) {
     return;
